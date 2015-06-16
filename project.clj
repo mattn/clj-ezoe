68de5085e34871b6d7fdfb5e3899d956043f05ff
mattn <mattn.jp@gmail.com>
2015-06-17 01:28:14 +0900

    質問ではない。

--- project.clj
+++ project.clj
@@ -1,9 +1,13 @@
 (defproject ezoe "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :description "質問ではない。"
+  :url "https://github.com/mattn/cl-ezoe"
+  :license {:name "MIT"
+            :url "http://mattn.mit-license.org/2015"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojars.scsibug/feedparser-clj "0.4.0"]
+                 [io.aviso/pretty "0.1.18"]
+                 [enlive "1.1.5"]
+                 [clj-http "1.1.2"]]
   :main ^:skip-aot ezoe.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/ezoe/core.clj
+++ src/ezoe/core.clj
@@ -1,7 +1,36 @@
 (ns ezoe.core
-  (:gen-class))
+  (:gen-class)
+  (:use net.cgrand.enlive-html)
+  (:import java.net.URL)
+  (:require [feedparser-clj.core :as feedparser]
+            [io.aviso.ansi :as ansi]
+            [clj-http.client :as client]))
+
+(defn list-items []
+  ((def feed (feedparser/parse-feed (str "http://ask.fm/feed/profile/" "EzoeRyou" ".rss")))
+  (doseq [entry (:entries feed)]
+    (println (ansi/white (:uri entry)))
+    (println (str "  " (ansi/blue (:title entry))))
+    (let [ask (:value (:description entry))]
+      (println (str "  " (clojure.string/replace ask
+                #"(質問ではない。?|不?自由)" #(ansi/red (%1 1))))))
+    (println))))
+
+(defn post-item [mes]
+  (binding [clj-http.core/*cookie-store* (clj-http.cookies/cookie-store)]
+    (let [token (-> "http://ask.fm/mattn_jp" URL. html-resource
+        (select [(attr= :name "authenticity_token")]) first :attrs :value)]
+      (println token)
+      (print (client/post "http://ask.fm/mattn_jp/questions/create"
+        {:form-params {
+             "authenticity_token" token
+             "question[question_text]" mes
+             "question[force_anonymous]" "force_anonymous"}}
+         :headers { "Referer" "http://ask.fm/mattn_jp" })))))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "質問ではない。"
   [& args]
-  (println "Hello, World!"))
+  (if (> (count args) 0)
+    (post-item (clojure.string/join "" args))
+    (list-items)))
