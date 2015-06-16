398d57d58ee07febe66d2e42a1d1e5440f08265a
mattn <mattn.jp@gmail.com>
2015-06-17 00:23:09 +0900

    first import

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ezoe
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/ezoe/core_test.clj
@@ -0,0 +1,7 @@
+(ns ezoe.core-test
+  (:require [clojure.test :refer :all]
+            [ezoe.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
