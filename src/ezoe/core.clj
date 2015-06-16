402da25c147c318bbca68181be52701fded4b12d
mattn <mattn.jp@gmail.com>
2015-06-17 02:49:14 +0900

    質問ではない。

--- src/ezoe/core.clj
+++ src/ezoe/core.clj
@@ -16,19 +16,23 @@
                 #"(質問ではない。?|不?自由)" #(ansi/red (%1 1))))))
     (println)))
 
+(defn parse-html [html]
+  (html-resource (java.io.StringReader. html)))
+
 ;;; FIXME:
 (defn post-item [mes]
   (binding [clj-http.core/*cookie-store* (clj-http.cookies/cookie-store)]
-    (let [token (-> "http://ask.fm/mattn_jp" URL. html-resource
-        (select [(attr= :name "authenticity_token")]) first :attrs :value)]
-      (println token)
-      (print (client/post "http://ask.fm/mattn_jp/questions/create"
-      ;(print (client/post "http://httpbin.org/post"
-        {:form-params {
-             "authenticity_token" token
-             "question[question_text]" mes
-             "question[force_anonymous]" "force_anonymous"}
-         :headers { "Referer" "http://ask.fm/mattn_jp" }})))))
+  (let [token (-> (first
+    (select (parse-html (get (client/get "http://ask.fm/mattn_jp") :body))
+      [(attr= :name "authenticity_token")])) :attrs :value)]
+    (println token)
+    (print (client/post "http://ask.fm/mattn_jp/questions/create"
+    ;(print (client/post "http://httpbin.org/post"
+      {:form-params {
+           "authenticity_token" token
+           "question[question_text]" mes
+           "question[force_anonymous]" "force_anonymous"}
+       :headers { "Referer" "http://ask.fm/mattn_jp" }})))))
 
 (defn -main
   "質問ではない。"
